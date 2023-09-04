7b03c7681c9241d7948f8c9fd7bbe5ead4f7432f
chris35469 <chris35469@gmail.com>
2023-09-03 23:37:18 -0500

    Add abiility to review session

--- js/core/index.js
+++ js/core/index.js
@@ -1,13 +1,14 @@
 import { BLE } from "./ble.js"
 import { Signal } from "./signal.js"
 import { Muse } from "./muse.js"
-
+import { Events } from "./events.js"
 
 let main = function(){
-    let muse_visualizer = new Muse("graph", window.innerWidth, 600, 256 * 2, 1)
+    let muse_visualizer = new Muse("graph", window.innerWidth, window.innerWidth * 0.6, 256 * 2, 1)
     let graph_handlers = {"muse": muse_visualizer}
     let signal_handler = new Signal(graph_handlers)
     let ble = new BLE(signal_handler.add_data.bind(signal_handler))
+    let events = new Events(muse_visualizer, ble)
 }
 
 main()
--- js/core/muse.js
+++ js/core/muse.js
@@ -1,37 +1,57 @@
+
+import { Data } from "./data.js";
+import { MuseSeries } from "./series_muse.js";
+
 export const Muse = class {
     constructor(div_id, width, height, max_data, time_interval=1) {
         this.width = width
         this.height = height
-        this.graph = new Rickshaw.Graph( {
+        this.sample_freq = 256
+        this.graph = new Rickshaw.Graph({
             element: document.querySelector(`#${div_id}`), 
             width: width, 
             height: height, 
             renderer: 'line',
-            series: new Rickshaw.Series.FixedDuration([
-                { name: 'TP9', color: 'steelblue' }, 
-                { name: 'TP10', color: 'lightblue' }, 
-                { name: 'AF7', color: 'gold' }, 
-                { name: 'AF8', color: 'red' }
-            
-            ], undefined, {
-                timeInterval: time_interval,
-                maxDataPoints: max_data,
-                timeBase: new Date().getTime() / 1000
-            }) 
+            series: MuseSeries(max_data, time_interval),
         });
         this.isChannelDataReady = {2: false, 16:false, 3: false, 17: false}
         this.recent_data_temp = {}
         this.is_active = true
+        this.is_local_recording = false
+        this.data = new Data("muse", {2: "TP9", 3: "TP10", 17:"AF8", 16:"AF7"}, this.sample_freq)
+        //this.init_events()
     }
+
+    toggle_local_recording() {
+        this.is_local_recording = !this.is_local_recording
+        //console.log(this.is_local_recording)
+    }
+
+    /*
+    init_events() {
+         // Start Local Store
+         document.getElementById("start_local_store").onclick = function (e) {
+            this.is_local_recording = !this.is_local_recording
+
+            if(this.is_local_recording) {
+                document.querySelector(`#status`).innerHTML = "recording"
+                document.querySelector(`#graph`).style.display = "block"
+                this.data.clear_data()
+            } else {
+                document.querySelector(`#status`).innerHTML = ""
+                document.querySelector(`#graph`).style.display = "none"
+                console.log(this.data.get_data())
+            }
+
+        }.bind(this);
+    }
+    */
 
     get_graph() {
         return this.graph
     }
 
     add_data(data, electrode) {
-        // format data if required
-        //console.log("muse vis", data)
-
         this.recent_data_temp[electrode] = data
         this.isChannelDataReady[electrode] = true
         this.update_graph()
@@ -65,6 +85,11 @@ export const Muse = class {
             for (let i in this.recent_data_temp[2]) {
                 this.graph.series.addData(this.get_formatted_data(i))
                 this.graph.render()
+            }
+
+            // record data session
+            if(this.is_local_recording) {
+                this.data.add_data(this.recent_data_temp)
             }
 
             // Flush old data
